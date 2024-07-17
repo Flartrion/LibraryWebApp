59b8aca61cdcef5d498c0f47d4759e84a3995111
Flartrion <vladkontsevykh+github@gmail.com>
2024-07-17 18:01:02 +0400

    Modernization pt. 38 - Login page and add item page are made to use form element properly. With this comes automatic validation, autocompletion and such. Also, submit on press of "enter"

--- frontend/src/listPages/itemPage/itemAddPage/itemAddPage.tsx
+++ frontend/src/listPages/itemPage/itemAddPage/itemAddPage.tsx
@@ -2,32 +2,78 @@ import { Button, Container, TextField } from "@mui/material";
 import { useEffect, useReducer, useState } from "react";
 import itemAddModel from "./itemAddModel";
 
-function itemAddPageReducer(state: any, action: any) {}
+const itemAddPageReducer: React.Reducer<any, any> = (
+  state: any,
+  action: any
+) => {};
 
 function ItemAddPage() {
   const [processing, setProcessing] = useState<Boolean>(false);
-  // const [itemAddPageState, dispatch] = useReducer(itemAddPageReducer, {
-  //   ...itemAddModel,
-  // });
+  const [itemAddPageState, dispatch] = useReducer(itemAddPageReducer, {
+    ...itemAddModel,
+  });
+
   useEffect(() => {
     return () => {};
   });
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <TextField label="ISBN" variant="standard" />
-      <TextField label="RLBC" variant="standard" />
-      <TextField label="Title" variant="standard" />
-      <TextField label="Authors" variant="standard" />
-      <TextField label="Type" variant="standard" />
-      <TextField label="Language" variant="standard" />
-      <TextField multiline aria-multiline label="Details" variant="standard" />
-      <Button variant="contained">Submit</Button>
-    </Container>
+    <form onSubmit={handleSubmit}>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <TextField
+          label="ISBN"
+          variant="standard"
+          value={itemAddPageState.isbn}
+        />
+        <TextField
+          label="RLBC"
+          variant="standard"
+          value={itemAddPageState.rlbc}
+        />
+        <TextField
+          label="Title"
+          variant="standard"
+          required
+          value={itemAddPageState.title}
+        />
+        <TextField
+          label="Authors"
+          variant="standard"
+          value={itemAddPageState.authors}
+        />
+        <TextField
+          label="Type"
+          variant="standard"
+          value={itemAddPageState.type}
+        />
+        <TextField
+          label="Language"
+          variant="standard"
+          value={itemAddPageState.language}
+        />
+        <TextField
+          multiline
+          aria-multiline
+          label="Details"
+          variant="standard"
+          value={itemAddPageState.details}
+        />
+        <Button type="submit" variant="contained">
+          Submit
+        </Button>
+      </Container>
+    </form>
   );
 }
 
--- frontend/src/login/loginPage.tsx
+++ frontend/src/login/loginPage.tsx
@@ -6,13 +6,16 @@ import {
   TextField,
 } from "@mui/material";
 import loginController from "./loginController";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GitHub } from "@mui/icons-material";
 
 function LoginPage() {
   const [processing, setProcessing] = useState(false);
 
-  function handleSubmit(e: React.MouseEvent) {
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
     setProcessing(true);
     const login = (document.getElementById("loginEmail") as HTMLInputElement)
       .value;
@@ -20,7 +23,7 @@ function LoginPage() {
       document.getElementById("loginPassword") as HTMLInputElement
     ).value;
     loginController.loginRequest(login, password);
-  }
+  };
 
   useEffect(() => {
     loginController.subscribeView("loginPage", setProcessing);
@@ -30,47 +33,53 @@ function LoginPage() {
   });
 
   return (
-    <Box
-      sx={{
-        marginLeft: "auto",
-        marginRight: "auto",
-        alignSelf: "center",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "stretch",
-        maxWidth: "800px",
-        minWidth: "200px",
-      }}
-    >
-      <Box sx={{ marginBottom: "100px" }} />
-      <TextField
-        id="loginEmail"
-        type="email"
-        autoComplete="email"
-        label="E-mail"
-        variant="outlined"
-      />
-      <TextField
-        id="loginPassword"
-        type="password"
-        autoComplete="current-password"
-        label="Password"
-        variant="outlined"
-      />
-      <Button variant="contained" onClick={handleSubmit}>
-        PROCEED
-      </Button>
-      <Button
-        onClick={() => {
-          location.href = "/login/github";
+    <form onSubmit={handleSubmit} noValidate>
+      <Box
+        sx={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          alignSelf: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch",
+          justifyContent: "space-evenly",
+          minHeight: "40vh",
+          maxWidth: "800px",
+          minWidth: "200px",
         }}
       >
-        <GitHub />
-      </Button>
-      <Backdrop open={processing}>
-        <CircularProgress />
-      </Backdrop>
-    </Box>
+        <TextField
+          id="loginEmail"
+          type="email"
+          autoComplete="email"
+          label="E-mail"
+          variant="outlined"
+          required
+        />
+        <TextField
+          id="loginPassword"
+          type="password"
+          autoComplete="current-password"
+          label="Password"
+          variant="outlined"
+          required
+        />
+        <Button type="submit" variant="contained">
+          PROCEED
+        </Button>
+
+        <Button
+          onClick={() => {
+            location.href = "/login/github";
+          }}
+        >
+          <GitHub />
+        </Button>
+        <Backdrop open={processing}>
+          <CircularProgress />
+        </Backdrop>
+      </Box>
+    </form>
   );
 }
 export default LoginPage;
